2457dd14f10a5cc40e88d864e1499de3082c3884
Mateusz Stankowski <m.stankowski16@gmail.com>
2022-03-25 22:46:03 +0100

    Version 1.0

---
+++ src/main/java/com/mattstansky/coronavirustracker/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.mattstansky.coronavirustracker.controller;
+
+import com.mattstansky.coronavirustracker.model.LocationStats;
+import com.mattstansky.coronavirustracker.service.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalCases);
+
+        return "home";
+    }
+}
